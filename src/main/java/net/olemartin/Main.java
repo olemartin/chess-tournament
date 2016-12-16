c6de6f082bfe93639417f5047d8dbd632caed485
selbekk <kristofer.selbekk@bekk.no>
2016-12-16 20:06:02 +0100

    Also, imports

--- src/main/java/net/olemartin/Main.java
+++ src/main/java/net/olemartin/Main.java
@@ -15,11 +15,15 @@ import net.olemartin.service.user.UserService;
 import net.olemartin.spring.MonradProfile;
 import net.olemartin.spring.Production;
 import net.olemartin.tools.GsonJSONProvider;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.env.AbstractEnvironment;
 
 import javax.annotation.Resource;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletRegistration;
+import java.util.EnumSet;
 import java.util.Map;
 
 
@@ -64,15 +68,15 @@ public class Main extends Application<MonradConfiguration> {
         websocket.addMapping("/push/*");
 
         // CORS support
-       final FilterRegistration.Dynamic cors =
-           environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
-       // Configure CORS parameters
-       cors.setInitParameter("allowedOrigins", "*"); // TODO: This probably needs fixing at some point
-       cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
-       cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*"); // TODO: This probably needs a more strict setting at some point
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
 
-       // Add URL mapping
-       cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 }
