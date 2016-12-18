881a07bf13e57345a8b787e96033484a1de4517b
selbekk <kristofer.selbekk@bekk.no>
2016-12-18 12:29:14 +0100

    Create a verification endpoint
    This commit creates a verification endpoint for checking whether
    or not the stored username / password is correct or not

--- src/main/java/net/olemartin/service/user/UserResource.java
+++ src/main/java/net/olemartin/service/user/UserResource.java
@@ -1,12 +1,14 @@
 package net.olemartin.service.user;
 
 
+import io.dropwizard.auth.Auth;
 import net.olemartin.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -29,5 +31,11 @@ public class UserResource {
     @POST
     public User register(User user) {
         return userService.createUser(user.getUsername(), user.getPassword(), user.getName());
+    }
+
+    @GET
+    @Path("/verify")
+    public User verify(@Auth User user) {
+        return user;
     }
 }
